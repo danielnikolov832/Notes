89852eb9e92ab4e6bd0cf3bb83fc2fd09f7d1822
Daniel Nikolov <danielsnikolov@gmail.com>
2022-01-24 20:55:50 +0200

    Changed subNotes collection to Hashset<Note> and added NoteEqualityComparer for implementation of custom equality logic

--- NotesLibrary/Source code/Note.cs
+++ NotesLibrary/Source code/Note.cs
@@ -1,3 +1,5 @@
+using System.Diagnostics.CodeAnalysis;
+
 namespace NotesLibrary
 {
     // The base form of a note, which is a holder of text information
@@ -6,31 +8,56 @@ namespace NotesLibrary
         private const string otherSubNotesWithTheSameNameString =
         "The argument 'name' is not valid, because there is a subNote with the same name";
 
-        private string name;
-        public string get_name { get => name; }
-
+        public string get_name { get; private set; }
         public string text;
+        public Note? get_parentNote { get; init; }
 
-        private Note? parentNote {get; init;}
+        public HashSet<Note> get_subNotes { get; init; }
+        private class NoteEqualityComparer : IEqualityComparer<Note>
+        {
+            public bool Equals(Note? x, Note? y)
+            {
+                if (x == null && y == null) return true;
 
-        public List<Note> get_subNotes { get; init; }
+                if (x == null || y == null) return false;
+
+                bool output_areWithSameName = x.get_name == y.get_name;
+
+                return output_areWithSameName;
+            }
+
+            public int GetHashCode([DisallowNullAttribute] Note obj)
+            {
+                return obj.get_name.GetHashCode(StringComparison.Ordinal);
+            }
+        }
+
+        // Creates an object, with all of the same fields and properties 
+        // as the copy object, but who is a different reference to it.
+        private Note(Note copy)
+        {
+            get_name = copy.get_name;
+            get_subNotes = copy.get_subNotes;
+            text = copy.text;
+            get_parentNote = copy.get_parentNote;
+        }
 
         public Note(string name, string text = "", Note? parentNote = null)
         {
-            this.name = name;
+            get_name = name;
             this.text = text;
-            this.parentNote = parentNote;
+            get_parentNote = parentNote;
             
-            get_subNotes = new List<Note>();
+            get_subNotes = new HashSet<Note>(new NoteEqualityComparer());
         }
 
         public Note(string name, IEnumerable<Note> subNotes, string text = "", Note? parentNote = null)
         {
-            this.name = name;
+            get_name = name;
             this.text = text;
-            this.parentNote = parentNote;
+            get_parentNote = parentNote;
 
-            this.get_subNotes = new List<Note>();
+            get_subNotes = new HashSet<Note>(new NoteEqualityComparer());
 
             foreach (Note noteItem in subNotes)
             {
@@ -40,23 +67,35 @@ namespace NotesLibrary
 
         public void SetName_OrThrow(string name)
         {
-            if (parentNote == null)
-            {
-                this.name = name;
+            bool isNoteValidWithNewName = IsNoteValidWithNewName(this, name);
 
-                return;
-            }
-            
-            bool isNoteNameValidForParentNote = parentNote.IsSubNoteNameValid(name);
-
-            if (isNoteNameValidForParentNote)
+            if (isNoteValidWithNewName)
             {
-                this.name = name;
+                this.get_name = name;
             }
             else
             {
                 throw new ArgumentException(otherSubNotesWithTheSameNameString);
             }
+        }
+
+        private static bool IsNoteValidWithNewName(Note note, string newName)
+        {   
+            Note noteWithNewName = new Note(note);
+            noteWithNewName.get_name = newName;
+
+            bool output_isNoteValidForParentNote = IsSubNoteValidForParentNote(noteWithNewName);
+
+            return output_isNoteValidForParentNote;
+        }
+
+        private static bool IsSubNoteValidForParentNote(Note subNote)
+        {
+            Note? parentNote = subNote.get_parentNote;
+
+            if (parentNote == null) return true;
+
+            return parentNote.get_subNotes.Contains(subNote);
         }
 
         public bool TrySetName(string name)
@@ -75,23 +114,12 @@ namespace NotesLibrary
 
         public void AddSubNote_OrThrow(Note subNote)
         {
-            bool isSubNoteValid = IsSubNoteValid(subNote);
+            bool isSubNoteAdded = get_subNotes.Add(subNote);
 
-            if (isSubNoteValid)
-            {
-                get_subNotes.Add(subNote);
-            }
-            else
+            if (isSubNoteAdded == false)
             {
                 throw new ArgumentException(otherSubNotesWithTheSameNameString);
             }
-        }
-
-        public void AddSubNoteFromNameAndText_OrThrow(string name, string text = "")
-        {
-            Note subNote = new Note(name, text);
-
-            AddSubNote_OrThrow(subNote);
         }
 
         public bool TryAddSubNote(Note subNote)
@@ -108,52 +136,46 @@ namespace NotesLibrary
             return true;
         }
 
-        public bool TryAddSubNoteFromNameAndText(string name, string subtext = "")
-        {
-            Note subNote = new Note(name, subtext);
-
-            bool output = TryAddSubNote(subNote);
-
-            return output;
-        }
-
-        private bool IsSubNoteValid(Note subNote)
-        {
-            bool output = IsSubNoteNameValid(subNote.name);
-
-            return output;
-        }
-
-        private bool IsSubNoteNameValid(string name)
+        public Note? GetSubNoteWithName(string name)
         {
             foreach (Note noteItem in get_subNotes)
             {
-                if (noteItem.name == name) return false;
+                if (noteItem.get_name == name) return noteItem;
             }
 
-            return true;
+            return null;
         }
 
         public bool TryRemoveSubNote(Note subNote)
         {
-            bool output = get_subNotes.Remove(subNote);
+            bool output_isRemoved = get_subNotes.Remove(subNote);
 
-            return output;
+            return output_isRemoved;
         }
 
         public bool TryRemoveSubNoteWithName(string name)
         {
             foreach (Note noteItem in get_subNotes)
             {
-                if (noteItem.name == name)
+                if (noteItem.get_name == name)
                 {
-                    bool output = get_subNotes.Remove(noteItem);
+                    bool output_isRemoved = get_subNotes.Remove(noteItem);
 
-                    return output;
+                    return output_isRemoved;
                 }
             }
 
             return false;
+        }
+
+        public void RemoveAllSubNotes()
+        {
+            RemoveAllSubNotesWhere( (Note obj) => { return true; } );
+        }
+
+        public void RemoveAllSubNotesWhere(Predicate<Note> match)
+        {
+            get_subNotes.RemoveWhere(match);
         }
     }
 }
