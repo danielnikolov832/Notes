68ff3bcd800f8661cd47dadac32064c7c07b7bce
Daniel Nikolov <danielsnikolov@gmail.com>
2022-01-28 16:42:21 +0200

    Changed logic to not encapsulate get_subNotes collection, and added event logic to facilitate that

--- NotesLibrary/Source code/Note.cs
+++ NotesLibrary/Source code/Note.cs
@@ -37,32 +37,13 @@ namespace NotesLibrary
                 OnPropertyChanged(nameof(get_parentNote));
             }
         }
-
                 
-        private NoteCollection get_subNotes { get; init; }
-        public IReadOnlyCollection<Note> get_subNotesAsReadOnly
-        {
-            get => (IReadOnlyCollection<Note>)get_subNotes;
-        }
+        public NoteCollection get_subNotes { get; init; }
 
         public event PropertyChangedEventHandler? PropertyChanged;
         protected virtual void OnPropertyChanged(string propertyName)
         {
             PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
-        }
-        public string Temp_GetEventInvocationListAsString()
-        {
-            string output = string.Empty;
-
-            if (PropertyChanged != null)
-            {
-                foreach (Delegate _delegate in PropertyChanged.GetInvocationList())
-                {
-                    output += _delegate.Method.Name + "\n";
-                }
-            }
-            
-            return output;
         }
 
         public Note(string name, string text = "")
@@ -99,76 +80,12 @@ namespace NotesLibrary
 
         protected virtual void get_SubNotes_NoteCollection_OnBeforeInsertOrSetItem(object? sender, Note e)
         {
-            e.get_parentNote = this;
+            e.parentNote = this;
         }
 
         protected virtual void get_SubNotes_NoteCollection_OnBeforeRemoveItem(object? sender, Note e)
         {
-            e.get_parentNote = null;
-        }
-
-        public void AddSubNote(Note subNote)
-        {
-            get_subNotes.Add(subNote);
-        }
-
-        public Note? GetSubNoteWithName(string name)
-        {
-            Note? output = get_subNotes.GetNoteWithName(name);
-
-            return output;
-        }
-
-        public bool RemoveSubNote(Note subNote)
-        {
-            bool output = get_subNotes.Remove(subNote);
-
-            return output;
-        }
-
-        public void RemoveSubNoteAtIndex(int index)
-        {
-            get_subNotes.RemoveAt(index);
-        }
-
-        public bool TryRemoveSubNoteAtIndex(int index)
-        {
-            try
-            {
-                RemoveSubNoteAtIndex(index);
-
-                return true;
-            }
-            catch(ArgumentException)
-            {
-                return false;
-            }
-        }
-
-        public bool TryRemoveSubNoteWithName(string name)
-        {
-            Note? subNote = GetSubNoteWithName(name);
-
-            if (subNote == null) return false;
-            
-            bool output = RemoveSubNote(subNote);
-
-            return output;
-        }
-
-        public void RemoveAllSubNotesWhere(Predicate<Note> condition)
-        {
-            get_subNotes.RemoveAllWhere(condition);
-        }
-
-        public void RemoveAllSubNotes()
-        {
-            get_subNotes.RemoveAll();
-        }
-
-        public override string ToString()
-        {
-            return name + " says " + text;
+            e.parentNote = null;
         }
     }
 }
