07c64e7e2ccfdfcc2b2966656de278d72b074c08
Daniel Nikolov <danielsnikolov@gmail.com>
2022-01-28 16:40:16 +0200

    Init to test NoteCollection type

---
+++ Testing/Source code/NoteCollectionTest.cs
@@ -0,0 +1,63 @@
+using NotesLibrary;
+
+namespace Testing
+{
+    internal static class NoteCollectionTest
+    {
+        private static void Main(string[] args)
+        {
+            TestConstruction();
+            TestMethods();
+        }
+
+        private static void TestConstruction()
+        {
+            NoteCollection noteCollection1 = new NoteCollection(); 
+
+            List<Note> notesList = new List<Note>()
+            {
+                new Note("Num 1"),
+                new Note("Num 2"),
+                new Note("Num 3")
+            };
+
+            noteCollection1.AddRange(notesList);            
+        }
+
+        private static void TestMethods()
+        {
+            NoteCollection noteCollection1 = new NoteCollection();
+
+            Note note1 = new Note("Num 1");
+            Note note2 = new Note("Num 2");
+            Note note3 = new Note("Num 3");
+
+            List<Note> notesList = new List<Note>()
+            {
+                note1,
+                note2,
+                note3
+            };
+
+            noteCollection1.TryAdd(note1);
+            noteCollection1.AddRange(notesList);
+
+            noteCollection1[0] = note1;
+
+            noteCollection1.Remove(note2);
+            noteCollection1.RemoveAt(1);
+
+            noteCollection1.GetNoteWithName("Num 1");
+
+            noteCollection1.TryRemoveNoteWithName("Num 1");
+
+            noteCollection1.AddRange(notesList);
+
+            noteCollection1.RemoveAllWhere((Note note) => {
+                return note.getset_name == "Num 3";
+            });
+
+            noteCollection1.RemoveAll();
+        }
+    }
+}
