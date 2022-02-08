f7892b4259e502e84c6622f763c1a327cbbbb299
Daniel Nikolov <danielsnikolov@gmail.com>
2022-02-08 18:58:42 +0200

    Changed according to changes in Note

--- Testing/Source code/NotesLibrary/NoteTest.cs
+++ Testing/Source code/NotesLibrary/NoteTest.cs
@@ -1,4 +1,4 @@
-﻿using NotesLibrary;
+﻿using NotesLibrary.Notes;
 
 namespace Testing.NotesLibrary
 {
@@ -7,27 +7,13 @@ namespace Testing.NotesLibrary
         static void Main(string[] args)
         {
             TestConstruction();
-            TestSubNotes();
         }
 
         private static void TestConstruction()
         {
-            Note note1 = new Note("Basic1", "Do 1");
+            Note note1 = new Note("Basic1");
 
-            Note note2 = new Note("Basic2", subNotes: new List<Note>() { note1 }, "Do 2");
-        }
-
-        private static void TestSubNotes()
-        {
-            Note note1 = new Note("Basic1", "Do 1");
-
-            Note note2 = new Note("Basic2", subNotes: new List<Note>() { note1 }, "Do 2");
-
-            Note note3 = new Note("Basic3", subNotes: new List<Note>(), "Do 3");
-
-            note1.get_subNotes.Add(note2);
-
-            note1.get_subNotes.Remove(note2);
+            Note note2 = new Note("Basic2", "Do 2");
         }
     }   
 }
